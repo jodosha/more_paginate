c06af942d00533a104b36679da359ff23a74a852
Luca Guidi <guidi.luca@gmail.com>
2009-10-31 12:22:33 +0100

    Specs for find conditions

--- lib/more_paginate.rb
+++ lib/more_paginate.rb
@@ -10,30 +10,55 @@ module MorePaginate
 
   module ClassMethods
     def paginate(scope, options = {})
-      options, collection_options = prepare_more_paginate_options(options)
+      options, collection_options = prepare_more_paginate_options!(options)
       MorePaginateCollection.new find(scope, options), collection_options
     end
 
     private
-      def prepare_more_paginate_options(options)
-        options[:order] ||= ""
-        options[:order] << ", id ASC"
-
-        options.merge!(:limit => per_page)
+      def prepare_more_paginate_options!(options)
+        add_more_paginate_order!(options)
+        add_more_paginate_limit!(options)
         collection_options = options.dup
+        add_more_paginate_conditions!(options)
 
+        [ options, collection_options ]
+      end
+
+      def add_more_paginate_order!(options)
+        # TODO use proper primary key instead of hardcoded 'id'
+        if options[:order].blank?
+          options[:order] = "id ASC"
+        else
+          options[:order] << ", id ASC"
+        end
+      end
+
+      def add_more_paginate_limit!(options)
+        options.merge!(:limit => per_page)
+      end
+
+      def add_more_paginate_conditions!(options)
         sort_key   = options.delete(:sort_key)
         sort_value = options.delete(:sort_value)
 
         if sort_value
-          options[:conditions] ||= []
-          # TODO use safe conditions
-          # options[:conditions] << ["BINARY(?) > BINARY(?)", sort_key, sort_value]
-          options[:conditions] << ["BINARY(#{sort_key}) > BINARY(?)", sort_value]
-          options[:conditions].flatten!
+          case options[:conditions]
+          when String
+            options[:conditions] = [ "#{options[:conditions]} AND #{more_paginate_condition_string}", sort_key, sort_value ]
+          when Array
+            options[:conditions][0] = "#{options[:conditions][0]} AND #{more_paginate_condition_string}"
+            options[:conditions] << [ sort_key, sort_value ]
+            options[:conditions].flatten!
+          when Hash
+            # TODO implement
+          else
+            options[:conditions] = [more_paginate_condition_string, sort_key, sort_value]
+          end
         end
+      end
 
-        [options, collection_options]
+      def more_paginate_condition_string
+        "BINARY(?) > BINARY(?)"
       end
   end
 
@@ -57,12 +82,14 @@ module MorePaginate
   end
 
   module Helpers
+    include Rack::Utils
+
     def more_paginate(records, options = {})
       # TODO prefix options
       options[:text]  ||= t(:more, :default => "more")
       options[:id]    ||= "more_link"
       options[:class] ||= "more_link"      
-      link_to h(options[:text]), "?sort_key=#{h(records.sort_key)}&sort_value=#{h(records.sort_value)}",
+      link_to h(options[:text]), "?sort_key=#{h(records.sort_key)}&sort_value=#{escape(records.sort_value)}",
         :id => options[:id], :class => options[:class], :"data-sort-value" => records.sort_value
     end
   end
---
+++ spec/activerecord/base_spec.rb
@@ -0,0 +1,116 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ActiveRecord::Base do
+  describe "per page" do
+    it "should have a class method" do
+      ActiveRecord::Base.per_page.should == 30
+    end
+
+    it "should be able to set custom value" do
+      Event.per_page.should == 23
+    end
+    
+    it "should overwrite superclass value" do
+      Festival.per_page.should == 11
+    end
+  end
+
+  describe "prepare_more_paginate_options" do
+    describe "order" do
+      it "should set default" do
+        options = { }
+        with_paginate_options options do |options, collection_options|
+          options[:order].should            == "id ASC"
+          collection_options[:order].should == "id ASC"
+        end
+      end
+
+      it "should add default to the already existing value" do
+        options = { :order => "name ASC" }
+        with_paginate_options options do |options, collection_options|
+          options[:order].should            == "name ASC, id ASC"
+          collection_options[:order].should == "name ASC, id ASC"
+        end
+      end
+    end
+
+    describe "limit" do
+      it "should force class value" do
+        options = { :limit => 1000 }
+        with_paginate_options options do |options, collection_options|
+          options[:limit].should            == Event.per_page
+          collection_options[:limit].should == Event.per_page
+        end
+      end
+    end
+
+    describe "sort_key" do
+      it "should remove from options" do
+        options = { :sort_key => "name" }
+        with_paginate_options options do |options, collection_options|
+          options[:sort_key].should               be_nil
+          collection_options[:sort_key].should == "name"
+        end
+      end
+    end
+
+    describe "sort_value" do
+      it "should remove from options" do
+        options = { :sort_value => "A Day To Remember live!" }
+        with_paginate_options options do |options, collection_options|
+          options[:sort_value].should               be_nil
+          collection_options[:sort_value].should == "A Day To Remember live!"
+        end
+      end
+    end
+
+    describe "conditions" do
+      it "should set default" do
+        options = { :sort_key => "name", :sort_value => "A Day To Remember live!" }
+        with_paginate_options options do |options, collection_options|
+          options[:conditions].should == ["BINARY(?) > BINARY(?)", "name", "A Day To Remember live!"]
+        end        
+      end
+
+      it "should preserve given string SQL conditions" do
+        options = { :conditions => "name IS NOT NULL", :sort_key => "name", :sort_value => "A Day To Remember live!" }
+        with_paginate_options options do |options, collection_options|
+          options[:conditions].should == ["name IS NOT NULL AND BINARY(?) > BINARY(?)", "name", "A Day To Remember live!"]
+        end
+      end
+
+      it "should preserve given array SQL conditions" do
+        options = { :conditions => ["id IS IN(?)", 23], :sort_key => "name", :sort_value => "A Day To Remember live!" }
+        with_paginate_options options do |options, collection_options|
+          options[:conditions].should == ["id IS IN(?) AND BINARY(?) > BINARY(?)", 23, "name", "A Day To Remember live!"]
+        end
+      end
+
+      it "should preserve given hash SQL conditions"
+      # it "should preserve given hash SQL conditions" do
+      #   options = { :conditions => { :id => 23 }, :sort_key => "name", :sort_value => "A Day To Remember live!" }
+      #   with_paginate_options options do |options, collection_options|
+      #     options[:conditions].should == ["id IS IN(?) AND BINARY(?) > BINARY(?)", 23, "name", "A Day To Remember live!"]
+      #   end
+      # end
+
+      it "shouldn't set if sort_value is missing" do
+        options = { :conditions => "name IS NOT NULL", :sort_key => "name" }
+        with_paginate_options options do |options, collection_options|
+          options[:conditions].should == "name IS NOT NULL"
+        end
+      end
+    end
+
+    describe "more_paginate_condition_string" do
+      it "have default value" do
+        Event.send(:more_paginate_condition_string).should == "BINARY(?) > BINARY(?)"
+      end
+    end
+  end
+
+  private
+    def with_paginate_options(options)
+      yield Event.send :prepare_more_paginate_options!, options
+    end
+end
