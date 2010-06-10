66b9e0e0f78b2ea897c094220cdb49cda452a381
Luca Guidi <guidi.luca@gmail.com>
2010-06-10 03:37:55 +0200

    Made the specs running for Rails 3 env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ require "rack"
 $:.unshift File.expand_path(File.dirname(__FILE__) + "/..")
 require "lib/more_paginate"
 load "init.rb"
-require "spec/fixtures"
 
 def create_tables
   ActiveRecord::Migration.verbose = false
@@ -61,3 +60,4 @@ end
 ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database  => ":memory:"
 ActiveRecord::Base.logger = ActiveSupport::BufferedLogger.new "log/database.log"
 create_tables
+require "spec/fixtures"
